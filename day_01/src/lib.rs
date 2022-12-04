07256d0279acde966863a08891436243bbad1850
Matt Mullins <mmullins@mmlx.us>
2022-12-03 23:54:30 -0800

    day 01: solved part 1

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -1,12 +1,42 @@
+use prelude::log::debug;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct Solution;
+pub struct Solution {
+    elves: Vec<Vec<u64>>,
+}
 
 #[wasm_bindgen]
 impl Solution {
-    pub fn new() -> Self {
+    pub fn new(input: &str) -> Self {
         prelude::init();
-        Solution
+
+        let mut elves = vec![];
+        let mut elf = vec![];
+
+        for line in input.lines() {
+            prelude::log::debug!("got: {:?}", line);
+
+            if line.is_empty() {
+                elves.push(elf);
+                elf = vec![];
+                continue;
+            }
+
+            elf.push(line.parse().expect("input was bad"));
+        }
+        elves.push(elf);
+
+        debug!("elves: {:?}", elves);
+
+        Solution { elves }
+    }
+
+    pub fn part1(&self) -> u64 {
+        self.elves
+            .iter()
+            .map(|elf| elf.iter().sum())
+            .max()
+            .expect("there weren't any elves?")
     }
 }
--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -1,6 +1,12 @@
 pub use prelude_2021::*;
 
+pub extern crate log;
+
 pub fn init() {
     console_error_panic_hook::set_once();
-    console_log::init().unwrap();
+    #[cfg(debug_assertions)]
+    let _ = console_log::init_with_level(log::Level::Debug);
+
+    #[cfg(not(debug_assertions))]
+    let _ = console_log::init();
 }
