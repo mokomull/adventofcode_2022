074e38e9d66ac4dca806543ee959cc1ee9c60d0f
Matt Mullins <mmullins@mmlx.us>
2022-12-04 20:43:50 -0800

    day 03: solve part 1

---
+++ day_03/src/lib.rs
@@ -0,0 +1,52 @@
+use prelude::log::debug;
+use prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Solution {
+    rucksacks: Vec<(Vec<u8>, Vec<u8>)>,
+}
+
+fn priority(c: u8) -> u64 {
+    match c {
+        b'a'..=b'z' => (c - b'a') as u64 + 1,
+        b'A'..=b'Z' => (c - b'A') as u64 + 27,
+        _ => panic!("unexpected letter {:?}", c),
+    }
+}
+
+#[wasm_bindgen]
+impl Solution {
+    pub fn new(input: &str) -> Self {
+        init();
+
+        let rucksacks = input
+            .lines()
+            .map(|line| {
+                let line = line.as_bytes();
+                let (first, second) = line.split_at(line.len() / 2);
+
+                (first.to_vec(), second.to_vec())
+            })
+            .collect();
+
+        debug!("rucksacks: {:x?}", rucksacks);
+
+        Self { rucksacks }
+    }
+
+    pub fn part1(&self) -> u64 {
+        self.rucksacks
+            .iter()
+            .map(|(first, second)| {
+                let first = first.iter().collect::<HashSet<_>>();
+                let second = second.iter().collect::<HashSet<_>>();
+
+                let overlap = first.intersection(&second).collect_vec();
+                assert_eq!(overlap.len(), 1);
+
+                priority(**overlap[0])
+            })
+            .sum()
+    }
+}
