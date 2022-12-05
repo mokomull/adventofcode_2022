bd7ddf1ee0735a3ed361c351c956e03cbbaa9ef6
Matt Mullins <mmullins@mmlx.us>
2022-12-04 21:15:44 -0800

    day 05: initial attempt at parsing
    This ... does not work.

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -3,13 +3,44 @@ use prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct Solution;
+pub struct Solution {
+    initial: Vec<Vec<u8>>, // stacks of crates from the bottom up
+    steps: Vec<(usize, usize, usize)>,
+}
 
 #[wasm_bindgen]
 impl Solution {
     pub fn new(input: &str) -> Self {
         init();
 
-        Solution
+        let lines = input.lines().collect_vec();
+        let separator_index = lines
+            .iter()
+            .position(|line| line.is_empty())
+            .expect("you should've given me an empty line at least");
+
+        let stack_count = lines[separator_index - 1]
+            .split(' ')
+            .filter(|word| !word.is_empty())
+            .count();
+        let mut stacks = vec![vec![]; stack_count];
+        for line in lines[..separator_index - 2].iter().rev() {
+            for stack_index in 0..stack_count {
+                match line.as_bytes().get(stack_index * 4 + 1) {
+                    None => continue,
+                    Some(c @ b'A'..=b'Z') => {
+                        stacks[stack_index].push(*c);
+                    }
+                    Some(_) => continue,
+                }
+            }
+        }
+
+        debug!("initial stacks: {:?}", stacks);
+
+        Solution {
+            initial: stacks,
+            steps: unimplemented!(),
+        }
     }
 }
