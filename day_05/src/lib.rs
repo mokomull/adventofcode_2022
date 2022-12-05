c00d360bed805349f8deba6c9115b06054645ea1
Matt Mullins <mmullins@mmlx.us>
2022-12-04 20:57:32 -0800

    day 05: copy over the daily skeleton

---
+++ day_05/src/lib.rs
@@ -0,0 +1,15 @@
+use prelude::log::debug;
+use prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Solution;
+
+#[wasm_bindgen]
+impl Solution {
+    pub fn new(input: &str) -> Self {
+        init();
+
+        Solution
+    }
+}
