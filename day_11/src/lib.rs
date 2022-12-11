0f33185fb0f747b917d462576a213fa843aed43d
Matt Mullins <mmullins@mmlx.us>
2022-12-10 23:21:08 -0800

    day 11: parse the monkeys

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -2,7 +2,13 @@ use prelude::log::debug;
 use prelude::*;
 use wasm_bindgen::prelude::*;
 
-struct Monkey {}
+struct Monkey {
+    items: Vec<u32>,
+    operation: Box<dyn Fn(u32) -> u32>,
+    divisor: u32,
+    true_target: usize,
+    false_target: usize,
+}
 
 #[wasm_bindgen]
 pub struct Solution {
@@ -12,6 +18,89 @@ pub struct Solution {
 #[wasm_bindgen]
 impl Solution {
     pub fn new(input: &str) -> Self {
-        unimplemented!()
+        init();
+
+        let mut lines = input.lines();
+        let mut monkeys = vec![];
+
+        while let Some(line) = lines.next() {
+            debug!("line: {:?}", line);
+
+            let monkey_id = line
+                .strip_prefix("Monkey ")
+                .expect("expected a monkey!")
+                .strip_suffix(":")
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+            // make sure we aren't given monkeys out of order
+            assert_eq!(monkey_id, monkeys.len());
+
+            let items = lines
+                .next()
+                .expect("unexpected EOF")
+                .strip_prefix("  Starting items: ")
+                .unwrap()
+                .split(", ")
+                .map(|word| word.parse().unwrap())
+                .collect();
+
+            let operation_str = lines
+                .next()
+                .expect("unexpected EOF")
+                .strip_prefix("  Operation: new = old ")
+                .unwrap();
+            let operation: Box<dyn Fn(u32) -> u32> = if operation_str == "* old" {
+                Box::new(|old| old * old)
+            } else if let Some(other) = operation_str.strip_prefix("* ") {
+                let other = other.parse::<u32>().unwrap();
+                Box::new(move |old| old * other)
+            } else if let Some(other) = operation_str.strip_prefix("+ ") {
+                let other = other.parse::<u32>().unwrap();
+                Box::new(move |old| old + other)
+            } else {
+                panic!("could not parse operation {:?}", operation_str);
+            };
+
+            let divisor = lines
+                .next()
+                .expect("unexpected EOF")
+                .strip_prefix("  Test: divisible by ")
+                .unwrap()
+                .parse()
+                .unwrap();
+
+            let true_target = lines
+                .next()
+                .expect("unexpected EOF")
+                .strip_prefix("    If true: throw to monkey ")
+                .unwrap()
+                .parse()
+                .unwrap();
+
+            let false_target = lines
+                .next()
+                .expect("unexpected EOF")
+                .strip_prefix("    If false: throw to monkey ")
+                .unwrap()
+                .parse()
+                .unwrap();
+
+            match lines.next() {
+                None => (),
+                Some("") => (),
+                Some(x) => panic!("expected empty line, got {:?}", x),
+            }
+
+            monkeys.push(Monkey {
+                items,
+                operation,
+                divisor,
+                true_target,
+                false_target,
+            })
+        }
+
+        Solution { monkeys }
     }
 }
