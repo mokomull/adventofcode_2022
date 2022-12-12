4d140c81bd248e6d1681b25958e221da9f8abdc6
Matt Mullins <mmullins@mmlx.us>
2022-12-11 20:01:05 -0800

    day 09: try to implement the moving rules
    This worked for the example, but not for my actual input

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -35,13 +35,15 @@ impl TryFrom<&str> for Move {
 }
 
 #[wasm_bindgen]
-pub struct Solution {}
+pub struct Solution {
+    directions: Vec<Move>,
+}
 
 #[wasm_bindgen]
 impl Solution {
     pub fn new(input: &str) -> Result<Solution, JsValue> {
         init();
-        
+
         let directions = input
             .lines()
             .map(Move::try_from)
@@ -49,6 +51,64 @@ impl Solution {
 
         debug!("parsed: {:#?}", directions);
 
-        Ok(Solution {})
+        Ok(Solution { directions })
+    }
+
+    pub fn part1(&self) -> usize {
+        let mut tail: (i32, i32) = (0, 0);
+        let mut head: (i32, i32) = (0, 0);
+        let mut visited = HashSet::new();
+
+        for instruction in &self.directions {
+            let (dx, dy, &count) = match instruction {
+                Up(x) => (0, 1, x),
+                Down(x) => (0, -1, x),
+                Left(x) => (-1, 0, x),
+                Right(x) => (1, 0, x),
+            };
+
+            for _ in 0..count {
+                head = (head.0 + dx, head.1 + dy);
+
+                if head.0 == tail.0 {
+                    // same row
+                    if head.1 > tail.1 + 1 {
+                        tail.1 += 1;
+                    } else if head.1 < tail.1 - 1 {
+                        tail.1 -= 1;
+                    }
+                } else if head.1 == tail.1 {
+                    // same column
+                    if head.0 > tail.0 + 1 {
+                        tail.0 += 1;
+                    } else if head.0 < tail.0 + 1 {
+                        tail.0 -= 1;
+                    }
+                } else {
+                    // something diagonal may happen
+                    if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
+                        // they're not touching, so tail needs to move diagonally
+                        
+                        // we know they're not in the same row or column if we get here, so these
+                        // will always move tail left-or-right *and* up-or-down.
+                        if head.0 > tail.0 {
+                            tail.0 += 1;
+                        } else {
+                            tail.0 -= 1;
+                        }
+
+                        if head.1 > tail.1 {
+                            tail.1 += 1;
+                        } else {
+                            tail.1 -= 1;
+                        }
+                    }
+                }
+
+                visited.insert(tail);
+            }
+        }
+
+        visited.len()
     }
 }
