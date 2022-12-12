f593bd21500240581ea0fe0b4848cbe9658d1685
Matt Mullins <mmullins@mmlx.us>
2022-12-11 18:21:59 -0800

    day 10: implement eval() for instructions
    I missed that it's 20, 60, 100, ... rather than every 20th cycle.
    
    I also missed that the "signal strength" is cycle * X, not just X.

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -32,6 +32,25 @@ impl TryFrom<&str> for Instruction {
     }
 }
 
+impl Instruction {
+    fn eval<F>(&self, x: i64, mut f: F) -> i64
+    where
+        F: FnMut(i64),
+    {
+        match self {
+            Noop => {
+                f(x);
+                x
+            }
+            Addx(value) => {
+                f(x);
+                f(x);
+                x + value
+            }
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub struct Solution {
     instructions: Vec<Instruction>,
@@ -50,5 +69,24 @@ impl Solution {
         debug!("parsed: {:#?}", instructions);
 
         Self { instructions }
+    }
+
+    pub fn part1(&self) -> i64 {
+        let mut x = 1;
+        let mut result = 0;
+        let mut cycle = 0;
+
+        for instruction in &self.instructions {
+            x = instruction.eval(x, |x| {
+                cycle += 1;
+
+                if cycle % 40 == 20 {
+                    debug!("cycle {}, x = {}", cycle, x);
+                    result += cycle * x;
+                }
+            })
+        }
+
+        result
     }
 }
