1a46ea7ff994d8a4a628f2bef5a3760d353d21dd
Matt Mullins <mmullins@mmlx.us>
2022-12-11 18:10:24 -0800

    day 10: parse the instructions

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -1,11 +1,35 @@
+use std::fmt::Debug;
+
 use prelude::log::debug;
 use prelude::*;
 use wasm_bindgen::prelude::*;
 
-#[derive(Clone)]
+use Instruction::*;
+
+#[derive(Debug)]
 enum Instruction {
     Noop,
     Addx(i64),
+}
+
+impl TryFrom<&str> for Instruction {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value == "noop" {
+            Ok(Noop)
+        } else if let Some(num) = value.strip_prefix("addx ") {
+            let operand = num
+                .parse()
+                .map_err(|e| format!("error parsing operand: {:?}", e))?;
+            Ok(Addx(operand))
+        } else {
+            Err(format!(
+                "expected noop or addx instruction, got {:?}",
+                value
+            ))
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -17,6 +41,14 @@ pub struct Solution {
 impl Solution {
     pub fn new(input: &str) -> Self {
         init();
-        unimplemented!()
+
+        let instructions = input
+            .lines()
+            .map(Instruction::try_from)
+            .collect::<Result<_, _>>()
+            .unwrap();
+        debug!("parsed: {:#?}", instructions);
+
+        Self { instructions }
     }
 }
