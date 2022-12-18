d45ed63810ad98773cef781cba20d4f1ec6dfd81
Matt Mullins <mmullins@mmlx.us>
2022-12-17 21:04:27 -0800

    day 18: parser

--- day_18/src/lib.rs
+++ day_18/src/lib.rs
@@ -3,13 +3,29 @@ use prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct Solution {}
+pub struct Solution {
+    cubes: Vec<(i32, i32, i32)>,
+}
 
 #[wasm_bindgen]
 impl Solution {
     pub fn new(input: &str) -> Self {
         init();
 
-        Solution {}
+        let cubes = input
+            .lines()
+            .map(|line| {
+                let split = line.split(',').collect_vec();
+                (
+                    split[0].parse().unwrap(),
+                    split[1].parse().unwrap(),
+                    split[2].parse().unwrap(),
+                )
+            })
+            .collect();
+
+        debug!("parsed {cubes:#?}");
+
+        Solution { cubes }
     }
 }
