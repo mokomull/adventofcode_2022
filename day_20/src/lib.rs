64a8a58d2e89d9542c6adc2701f1fc4b67067b11
Matt Mullins <mmullins@mmlx.us>
2022-12-19 23:02:20 -0800

    day 20: naively implement the shuffling algorithm
    This ... does not actually return the right answer, so it's time to
    debug.

--- day_20/src/lib.rs
+++ day_20/src/lib.rs
@@ -15,6 +15,37 @@ impl Solution {
         let input = input.lines().map(|line| line.parse().unwrap()).collect();
         debug!("parsed: {input:?}");
 
-        Solution {input}
+        Solution { input }
+    }
+
+    pub fn part1(&self) -> i32 {
+        // Vec of references so we can always find *exactly* the one we're looking for.
+        let mut arrangement: Vec<&_> = self.input.iter().collect_vec();
+
+        for number in &self.input {
+            let position = arrangement
+                .iter()
+                .position(|&element| std::ptr::eq(element, number))
+                .expect("somehow lost a thing");
+            arrangement.remove(position);
+
+            let target = (position as i32) + *number;
+            // the % operator always returns something the same sign as its left-hand side
+            let target = target % (self.input.len() as i32);
+            let target = if target < 0 {
+                target + (self.input.len() as i32)
+            } else {
+                target
+            };
+
+            arrangement.insert(
+                target.try_into().expect("target shouldn't be negative..."),
+                number,
+            );
+        }
+
+        debug!("final arrangement is {arrangement:?}");
+
+        unimplemented!()
     }
 }
