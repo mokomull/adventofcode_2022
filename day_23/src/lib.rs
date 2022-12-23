3a7870edc4b6af662216c86b726db70bc08fc481
Matt Mullins <mmullins@mmlx.us>
2022-12-22 21:20:59 -0800

    day 23: parser

--- day_23/src/lib.rs
+++ day_23/src/lib.rs
@@ -3,12 +3,30 @@ use prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct Solution {}
+pub struct Solution {
+    elves: HashSet<(i32, i32)>,
+}
 
 #[wasm_bindgen]
 impl Solution {
     pub fn new(input: &str) -> Self {
         init();
-        Self {}
+
+        let elves = input
+            .lines()
+            .enumerate()
+            .flat_map(|(row, line)| {
+                line.chars().enumerate().filter_map(move |(column, c)| {
+                    if c == '#' {
+                        Some((row as i32, column as i32))
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect();
+        debug!("parsed: {elves:#?}");
+
+        Self { elves }
     }
 }
